cd95a9c2173b68e72b8679c99eab018bdef9cb2b
Florian CHANCELADE <florian.chancelade@gmail.com>
2021-08-24 23:44:51 +0200

    feat: adding btn for add an input zone and delete input zone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import "./style.css";
+
+
+const btnMain = document.querySelector('#btn-main');
+const addBtnZone = document.querySelector('#btn-add-zone');
+
+
+btnMain.addEventListener('click', () => {
+    if (btnMain.dataset.btn === "add"){
+        btnMain.dataset.btn = "remove";
+        btnMain.className = "btn-remove";
+        btnMain.textContent = "x";
+        
+        const divInput = document.createElement("div");
+        divInput.className = "text-center row";
+        divInput.id = "input-zone";
+        addBtnZone.after(divInput);
+
+        var div = document.createElement("div");
+        div.className = "col col-lg-8";
+        divInput.append(div);
+
+        const inputElt = document.createElement("input");
+        inputElt.type = 'text';
+        inputElt.id = "input-text";
+        inputElt.className = "form-control";
+        div.append(inputElt);
+
+        var div = document.createElement("div");
+        div.className = "col col-lg-2";
+        divInput.append(div)
+        
+        const submitElt = document.createElement("button");
+        submitElt.textContent = "+";
+        submitElt.id = "submit-input";
+        submitElt.className = "col";
+        div.append(submitElt);
+
+    } else {
+        btnMain.dataset.btn = "add";
+        btnMain.className = "btn-add";
+        btnMain.textContent = "+";
+        const divInput = document.querySelector("#input-zone");
+        divInput.remove();
+    }
+})
