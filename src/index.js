9b06898a98a32fbb4007ce57c68fa3dc5f37fd5a
Florian CHANCELADE <florian.chancelade@gmail.com>
2021-08-25 22:59:26 +0200

    adding task OK | remove task OK

--- src/index.js
+++ src/index.js
@@ -1,39 +1,76 @@
 import "./style.css";
 
-const todoZone = document.querySelector('#todo-zone');
-const btnAdd = document.querySelector('#btn-add');
-const myModal = document.querySelector('#modal');
-const modalContent = document.querySelector('.modal-content');
+const todoZone = document.querySelector("#todo-zone");
+const btnOpenModal = document.querySelector("#btn-open-modal");
+const myModal = document.querySelector("#modal");
+const btnAdd = document.querySelector("#btn-add");
+
 function addTodo(text) {
+  const tr = document.createElement("tr");
+  const textTd = document.createElement("td");
+  textTd.className = "center todo";
+  textTd.textContent = text;
 
-    todoZone.innerHTML = `
-        <tr>
-            <td class="center todo">${text}</td>
-            <td class="center status">Finir le todo</td>
-            <td class="center">
-                <button class="btn edit">
-                    <i class="material-icons">edit</i>
-                </button>
-            </td>
-            <td class="center">
-                <button class="btn remove">
-                    <i class="material-icons">delete</i>
-                </button>
-            </td>
-        </tr>`
+  const statusTd = document.createElement("td");
+  statusTd.className = "center status";
+  statusTd.textContent = "status";
+
+  const editTd = document.createElement("td");
+  editTd.className = "center";
+
+  const buttonEdit = document.createElement("button");
+  buttonEdit.className = "btn";
+
+  const iconEditBtn = document.createElement("i");
+  iconEditBtn.className = "material-icons";
+  iconEditBtn.textContent = "edit";
+
+  buttonEdit.append(iconEditBtn);
+  editTd.append(buttonEdit);
+
+  const removeTd = document.createElement("td");
+  removeTd.className = "center";
+
+  const buttonRemove = document.createElement("button");
+  buttonRemove.className = "btn red";
+
+  buttonRemove.addEventListener("click", () => {
+    tr.remove();
+  });
+
+  const iconRemoveBtn = document.createElement("i");
+  iconRemoveBtn.className = "material-icons";
+  iconRemoveBtn.textContent = "delete";
+
+  buttonRemove.append(iconRemoveBtn);
+  removeTd.append(buttonRemove);
+
+  tr.append(textTd);
+  tr.append(statusTd);
+  tr.append(editTd);
+  tr.append(removeTd);
+  todoZone.append(tr);
 }
 
-function modal(contain){
-    console.log('modal');
-    myModal.style.display = "block";
-
+function modal(contain) {
+  myModal.style.display = "block";
 }
 
-btnAdd.addEventListener('click', modal);
-console.log(btnAdd);
+btnOpenModal.addEventListener("click", modal);
 
-document.addEventListener('click', (event)=>{
-    if (event.target == myModal){
-        myModal.style.display = "None"
-    }
-})
+document.addEventListener("click", (event) => {
+  if (event.target == myModal) {
+    myModal.style.display = "None";
+  }
+});
+
+btnAdd.addEventListener("click", (event) => {
+  event.preventDefault();
+  const text = document.querySelector("#input-add");
+  const textValue = text.value.trim();
+  if (textValue) {
+    addTodo(textValue);
+    text.value = "";
+    myModal.style.display = "None";
+  }
+});
